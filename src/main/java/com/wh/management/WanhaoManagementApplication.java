071508598ed3da2fb0de51cec833f87189c8b055
linxibiao <linxibiao@meituan.com>
2020-03-26 19:11:06 +0800

    init

---
+++ src/main/java/com/wh/management/WanhaoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wh.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanhaoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanhaoManagementApplication.class, args);
+	}
+
+}
